8c5bf5ebf068f07e68e94c8725ce9fd3dc67f414
Justin Madilia <jmadilia96@gmail.com>
2025-04-02 15:16:10 -0400

    fix(auth): Fix build-breaking warnings resulting from improperly handled params on forgot-password page.

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -5,11 +5,13 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-export default function ForgotPassword({
+export default async function ForgotPassword({
   searchParams,
 }: {
-  searchParams: Message;
+  searchParams: Promise<Message>;
 }) {
+  const resolvedSearchParams = await searchParams;
+
   return (
     <>
       <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
@@ -28,7 +30,7 @@ export default function ForgotPassword({
           <SubmitButton formAction={forgotPasswordAction}>
             Reset Password
           </SubmitButton>
-          <FormMessage message={searchParams} />
+          <FormMessage message={resolvedSearchParams} />
         </div>
       </form>
     </>
